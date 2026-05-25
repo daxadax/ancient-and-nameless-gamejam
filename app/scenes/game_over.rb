9e0fa64fbbffa49483247d2ea3d097fff561efb0
dax <d.dax@email.com>
2026-05-25 12:55:57 +0200

    Simplify game over

--- app/scenes/game_over.rb
+++ app/scenes/game_over.rb
@@ -7,15 +7,10 @@ module Scenes
     def tick(args)
       draw_background_color(args)
 
-      if args.state.won
-        text = 'You win!'
-        draw_label(args, { x: 640, y: 360, text: text, size_px: 36 }, color: COLOR_GREEN)
-      else
-        text = "You lose! I am ancient & nameless!"
-        draw_label(args, { x: 640, y: 360, text: text, size_px: 36 }, color: COLOR_RED)
-      end
-
       # TODO: handle play again or meta-progression
+      # this is likely not game over but it's a reasonable placeholder for now
+      text = 'Game over!'
+      draw_title(args, { x: 640, y: 360, text: text, size_px: 36, color: RGB_RED })
     end
   end
 end
