850ac31edaecdb7960e6dace3bf7ca3215b8ac14
dax <d.dax@email.com>
2026-05-30 18:18:51 +0200

    Clean up before pivot

--- lib/event_deck.rb
+++ lib/event_deck.rb
@@ -3,21 +3,15 @@ require 'data/events/cult'
 module EventDeck
   def self.draw(args)
     run = args.state.run
+    events = Events::Cult.all
 
-    pool = Events::Cult.all.select { |event| eligible?(run, event) }
-    pool = Events::Cult.all if pool.empty?
-
-    pool.sample
+    events.select { |x| eligible?(run, x) }.sample
   end
 
   def self.eligible?(run, event)
     return false if run.events_seen.include?(event.id)
+    return false if run.act != event.act
 
-    # TODO: later
-    # act = event.act
-    # return false if act && run.act != act
-
-    # min_threat = event.min_threat || 0
-    # run.threat >= min_threat
+    true
   end
 end
