4007371a3b046f636b2831cf1482b2879024ae43
dax <d.dax@email.com>
2026-06-09 00:26:39 +0200

    simplified HUD

--- lib/ui.rb
+++ lib/ui.rb
@@ -7,15 +7,20 @@ module UI
   def draw_hud(run)
     draw_label(
       args,
-      { x: 40, y: 600, text: "Day #{run.day} of #{run.max_days}", size_px: 22 },
+      {
+        x: FULL_WIDTH - 120,
+        y: FULL_HEIGHT - 50,
+        text: "Day #{run.day} of #{run.max_days}",
+        size_px: 22
+      },
       color: RGB_INK
     )
 
-    meters = run.meters
-    meter_text = "Vibes #{meters.vibes} "
-    meter_text += "Food #{meters.food} "
-    meter_text += "Cleanliness #{meters.cleanliness} "
-    meter_text += "Authenticity #{meters.authenticity}"
-    draw_label(args, { x: 40, y: 575, text: meter_text, size_px: 18 }, color: RGB_BODY)
+    # meters = run.meters
+    # meter_text = "Vibes #{meters.vibes} "
+    # meter_text += "Food #{meters.food} "
+    # meter_text += "Cleanliness #{meters.cleanliness} "
+    # meter_text += "Authenticity #{meters.authenticity}"
+    # draw_label(args, { x: 40, y: 575, text: meter_text, size_px: 18 }, color: RGB_BODY)
   end
 end
