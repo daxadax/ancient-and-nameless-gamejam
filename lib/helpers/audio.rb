416c026c53d66740b6fd4a1757017b123e76c6a2
dax <d.dax@email.com>
2026-06-18 22:05:49 +0200

    Add fix for web audio

--- lib/helpers/audio.rb
+++ lib/helpers/audio.rb
@@ -3,34 +3,37 @@ require 'lib/campaign'
 module Audio
   MUSIC_KEY = :music
   MUSIC_PATH = 'sounds/headscratcher.ogg'
-  COIN_RATTLE_PATH = 'sounds/coins-rattling.mp3'
-  COIN_CLINK_PATH = 'sounds/coin-4.mp3'
+  COIN_RATTLE_PATH = 'sounds/coins-rattling.ogg'
+  COIN_CLINK_PATH = 'sounds/coin-4.ogg'
   COIN_RATTLE_GAIN = 0.72
   COIN_CLINK_GAIN = 0.55
-  CLICK_GAIN = 0.28
+  CLICK_GAIN = 0.35
 
   STEP = 0.1
 
   def self.tick!(args)
+    unlock_web_audio!(args)
+
     gain = music_gain(args)
     track = args.audio[MUSIC_KEY]
 
-    return start_music!(args) if track.nil? && gain.positive?
+    return start_music!(args) if track.nil? && gain.positive? && web_audio_ready?(args)
 
     return unless track
 
-    track.gain = gain
+    track.gain = gain.to_f
     stop_music!(args) if gain.zero?
   end
 
   def self.start_music!(args)
     gain = music_gain(args)
     return if gain.zero?
+    return unless web_audio_ready?(args)
 
     args.audio[MUSIC_KEY] = {
       input: MUSIC_PATH,
       looping: true,
-      gain: gain
+      gain: gain.to_f
     }
   end
 
@@ -38,46 +41,16 @@ module Audio
     args.audio[MUSIC_KEY] = nil
   end
 
-  def self.play_sfx!(args, path)
-    gain = sfx_gain(args)
-    return if gain.zero?
-
-    args.audio["sfx_#{Kernel.tick_count}".to_sym] = {
-      input: path,
-      gain: gain
-    }
-  end
-
   def self.play_click!(args)
-    gain = sfx_gain(args)
-    return if gain.zero?
-
-    args.audio["click_#{Kernel.tick_count}".to_sym] = {
-      input: COIN_CLINK_PATH,
-      gain: gain * CLICK_GAIN,
-      pitch: 1.4
-    }
+    play_one_shot!(args, COIN_CLINK_PATH, gain: CLICK_GAIN)
   end
 
   def self.play_coin_rattle!(args)
-    gain = sfx_gain(args)
-    return if gain.zero?
-
-    args.audio[:coin_rattle] = {
-      input: COIN_RATTLE_PATH,
-      gain: gain * COIN_RATTLE_GAIN
-    }
+    play_one_shot!(args, COIN_RATTLE_PATH, gain: COIN_RATTLE_GAIN)
   end
 
   def self.play_coin_clink!(args, index: 0)
-    gain = sfx_gain(args)
-    return if gain.zero?
-
-    path = COIN_CLINK_PATH
-    args.audio["coin_clink_#{Kernel.tick_count}_#{index}".to_sym] = {
-      input: path,
-      gain: gain * COIN_CLINK_GAIN
-    }
+    play_one_shot!(args, COIN_CLINK_PATH, gain: COIN_CLINK_GAIN)
   end
 
   def self.preview_sfx!(args)
@@ -107,11 +80,42 @@ module Audio
     return start_music!(args) unless track
 
     gain = music_gain(args)
-    track.gain = gain
+    track.gain = gain.to_f
     stop_music!(args) if gain.zero?
   end
 
   def self.volume_label(volume)
     "#{(volume * 100).round}%"
+  end
+
+  def self.web_audio_ready?(args)
+    return true unless $gtk.platform?(:web)
+
+    args.state.web_audio_ready
+  end
+
+  def self.unlock_web_audio!(args)
+    return if web_audio_ready?(args)
+    return unless web_user_input?(args)
+
+    args.state.web_audio_ready = true
+    start_music!(args) if args.audio[MUSIC_KEY].nil? && music_gain(args).positive?
+  end
+
+  def self.web_user_input?(args)
+    inputs = args.inputs
+    return true if inputs.mouse.up || inputs.mouse.down
+
+    inputs.keyboard.key_down.truthy_keys.any?
+  end
+
+  def self.play_one_shot!(args, path, gain:)
+    level = sfx_gain(args)
+    return if level.zero?
+
+    args.outputs.sounds << {
+      path: path,
+      gain: (level * gain).to_f
+    }
   end
 end
